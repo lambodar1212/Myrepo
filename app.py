fa4545e882fb28a4c7feaa44645453ae934cdb12
lambodar1212 <lambodarsagadia9@gmail.com>
2025-04-24 23:09:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import json
+import random
+
+app = Flask(__name__)
+
+# Load Shayari from JSON
+with open("shayari.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Welcome to Love Shayari API"})
+
+@app.route("/api/shayari", methods=["GET"])
+def get_shayari():
+    shayari = random.choice(data["shayaris"])
+    return jsonify({"shayari": shayari})
+
+if __name__ == "__main__":
+    app.run(debug=True)
